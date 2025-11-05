418b86cd1490da8d3bc4babb3535fb188332484a
Takagi Tasuku <tasuren@outlook.jp>
2025-11-05 23:26:06 +0900

    feat: add port of `CefScopedSendingEvent`

--- cef/src/application_mac.rs
+++ cef/src/application_mac.rs
@@ -1,4 +1,7 @@
-use objc2::{extern_protocol, runtime::Bool};
+use objc2::{
+    extern_protocol, rc::Retained, runtime::Bool, ClassType, DowncastTarget, MainThreadMarker,
+};
+use objc2_app_kit::NSApp;
 
 extern_protocol!(
     /// The binding of `CrAppProtocol`.
@@ -23,3 +26,49 @@ extern_protocol!(
     #[allow(clippy::missing_safety_doc)]
     pub unsafe trait CefAppProtocol: CrAppControlProtocol {}
 );
+
+/// It controls the state of `isHandlingSendEvent` on the object that implements [`CefAppProtocol`]
+/// in the event loop to ensure that it resets properly.
+///
+/// It should be called during performing `-[NSApplication sendEvent:]`.
+///
+/// # Examples
+/// ```rust
+/// impl SimpleApplication {
+///     #[unsafe(method(sendEvent:))]
+///     fn send_event(&self, event: &NSEvent) {
+///         // SimpleApplication must implements `CefAppProtocol`.
+///         cef::application_mac::scoped_sending_event::<SimpleApplication, _>(|| {
+///             // Perform `-[NSApplication sendEvent:]`.
+///             let _: () = unsafe { msg_send![super(self), sendEvent: event] };
+///         });
+///     }
+/// }
+/// ```
+///
+/// # Panics
+///
+/// This may panic if you don't call on main thread,
+/// or `NSApp` is not the type of the generics `App`.
+pub fn scoped_sending_event<App, T>(f: impl FnOnce() -> T) -> T
+where
+    // The struct type is needed to ensure that `NSApp` implements `CefAppProtocol`.
+    App: ClassType + DowncastTarget + CefAppProtocol,
+{
+    // This function is port of `CefScopedSendingEvent` class.
+
+    let mtm = MainThreadMarker::new().expect("This function must be called on main thread.");
+    let app: Retained<App> = NSApp(mtm)
+        .downcast()
+        .expect("`NSApp` is not the type of the generics `App`.");
+
+    unsafe {
+        let handling = app.is_handling_send_event();
+        app.set_handling_send_event(Bool::YES);
+
+        let result = f();
+        app.set_handling_send_event(handling);
+
+        result
+    }
+}
